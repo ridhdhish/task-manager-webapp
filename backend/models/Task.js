7a6765596a0f946f072d84d558f1cc0630087493
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-02-16 09:59:56 +0530

    Task schema craeted

---
+++ backend/models/Task.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    maxLength: [100, "Title can be of maximum 100 characters"],
+  },
+  description: {
+    type: String,
+  },
+  dueDate: {
+    type: Date,
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
